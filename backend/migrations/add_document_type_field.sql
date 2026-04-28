4c064dac4ef1941b3bfb809a14518462d7bbda74
“灵主” <123456@123.com>
2026-04-28 19:14:30 +0800

    默认身份审核以前的数据是身份证

--- backend/migrations/add_document_type_field.sql
+++ backend/migrations/add_document_type_field.sql
@@ -1,2 +1,8 @@
 ALTER TABLE "User"
 ADD COLUMN IF NOT EXISTS "documentType" TEXT;
+
+-- 已有身份证图片的用户默认设为 id_card
+UPDATE "User"
+SET "documentType" = 'id_card'
+WHERE "documentType" IS NULL
+  AND ("idCardFront" IS NOT NULL OR "idCardBack" IS NOT NULL);
